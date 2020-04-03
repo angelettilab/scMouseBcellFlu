c842a0aec5a25363699b936c7faf556c9b8865f6
Jonathan Robinson <jonrob@chalmers.se>
2020-04-03 16:14:05 +0200

    feat: files defining environment for trajectory analysis

---
+++ envs/trajectory/build_singularity_image.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+# To build remotely, you need to first connect to a sylabs remote server using a token.
+# Run the following command:
+#
+#singularity remote login
+#
+# Then paste the API key (in the "sylabs-token" file) into the terminal when prompted.
+# You should see the message "API Key Verified!".
+
+# build image
+singularity build --remote trajectory.sif trajectory_singularity.def
