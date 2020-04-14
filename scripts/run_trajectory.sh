c2dbf8bce72bb4781521354fe2fc28f0b407074b
Jonathan Robinson <jonrob@chalmers.se>
2020-04-14 13:06:49 +0200

    update run_trajectory.sh script

--- scripts/run_trajectory.sh
+++ scripts/run_trajectory.sh
@@ -3,14 +3,12 @@
 ########################
 ### DEFINE VARIABLES ###
 ########################
-# main='/home/jonrob/projects/d_angeletti_1910'
-# script_path='/home/jonrob/repos/sauron/scripts'
-main='/Users/jonrob/Documents/NBIS/LTS_projects/d_angeletti_1910/TEST'
-script_path='/Users/jonrob/Documents/NBIS/repos/sauron/scripts'
+main='/home/jonrob/projects/d_angeletti_1910'
+# main='/Users/jonrob/Documents/NBIS/LTS_projects/d_angeletti_1910'
 
 cd $main
-mkdir analysis
-mkdir log
+# mkdir analysis
+# mkdir log
 
 
 
@@ -18,30 +16,30 @@ mkdir log
 ### ACTIVATE CONDA ENVIRONMENT ###
 ##################################
 # conda activate trajectory-env  # macOS (local)
-source activate trajectory-env  # linux/unix (cluster)
+# source activate trajectory-env  # linux/unix (cluster)
 
 
 
 ################################
 ### RUN TRAJECTORY INFERENCE ###
 ################################
-# Rscript $main/scripts/trajectory_inference.R \
-Rscript $main/../scripts/trajectory_inference.R \
---Seurat_object_path $main/'analysis/02_cluster/seurat_object.rds' \
+# Rscript $main/../scripts/trajectory_inference.R \
+Rscript $main/scripts/trajectory_inference.R \
+--Seurat_object_path $main/'analysis/06_trajectory/seurat_object.rds' \
 --reduction_use 'dm' \
 --reduction_visualize 'umap' \
---cluster_use 'louvain_0.45' \
---start_cluster '0' \
+--cluster_use 'louvain_0.8' \
+--start_cluster '1' \
 --end_cluster 'none' \
 --diff_testing 'true' \
 --assay 'RNA' \
---output_path $main/'analysis/X_trajectory' \
+--output_path $main/'analysis/06_trajectory' \
 2>&1 | tee $main/log/'trajectory_log.txt'
 
 
 
 # deactivate conda environment
-conda deactivate
+# conda deactivate
 
 
 
