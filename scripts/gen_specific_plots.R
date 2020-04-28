fcc13d55e18f8b1f21a4e3bf6c0901a017dbe898
Jonathan Robinson <jonrob@chalmers.se>
2020-04-28 12:55:40 +0200

    feat: gen_specific_plots add more plots for trajectory analysis

--- scripts/gen_specific_plots.R
+++ scripts/gen_specific_plots.R
@@ -222,30 +222,92 @@ rm(temp)
 ### TRAJECTORY INFERENCE ###
 ############################
 
-tdir <- paste0(adir, '06_trajectory/')  # trajectory analysis directory
-clustering_use <- 'louvain_0.8'  # cluster grouping to show on plots
+# specify directories and clustering type(s)
+cdir <- paste0(adir, '04_cluster/')  # clustering analysis direcotry
+tdirs <- dir(adir, '^trajectory_\\d')  # trajectory analysis directories
+clustering_use <- 'louvain_0.95'  # cluster grouping to show on plots
 
+# create trajectory_summary directory if it does not exist
+summary_dir <- paste0(adir, 'trajectory_summary/')
+if (!dir.exists(summary_dir)) {dir.create(summary_dir)}
+
+# initialize summary table
+table_cols <- c('ID', 'Clustering', 'Clusters', 'Start', 'End')
+traj_summary_tab <- data.frame(matrix(nrow=length(tdirs), ncol=length(table_cols))) %>% setNames(table_cols)
+traj_summary_tab$ID <- tdirs
+traj_summary_tab$Clustering <- clustering_use  # same clustering for all
+                             
 # load Seurat object
-DATA <- readRDS(paste0(tdir, 'seurat_object.rds'))
+DATA <- readRDS(paste0(cdir, 'seurat_object.rds'))
+       
+# loop through each trajectory result subdirectory
+for (i in seq(length(tdirs))) {
+  tdir <- paste0(adir, tdirs[i], '/')
+  
+  # load lineages object and extract run information
+  lineages <- readRDS(paste0(tdir, 'lineages_object.rds'))
+  clusters <- colnames(lineages@clusterLabels)
+  traj_summary_tab$Clusters[i] <- clusters[order(as.numeric(clusters))] %>% paste0(collapse=',')
+  if (any(lineages@slingParams$start.given)) {
+    start_clusters <- lineages@slingParams$start.clus[lineages@slingParams$start.given]
+    traj_summary_tab$Start[i] <- start_clusters[order(as.numeric(start_clusters))] %>% paste0(collapse=',')
+  } else {
+    traj_summary_tab$Start[i] <- 'auto'
+  }
+  if (any(lineages@slingParams$end.given)) {
+    end_clusters <- lineages@slingParams$end.clus[lineages@slingParams$end.given]
+    traj_summary_tab$End[i] <- end_clusters[order(as.numeric(end_clusters))] %>% paste0(collapse=',')
+  } else {
+    traj_summary_tab$End[i] <- 'auto'
+  }
 
-# generate umap plot to show first
-umap_plot <- list(DimPlot(DATA, dims=1:2, reduction='umap', group.by=clustering_use, pt.size=0.1, label=T) +
-  theme(legend.position='none'))
+  # load diffusion map coordinates
+  dm_filename <- paste0(tdir, 'diffusion_map_coords.csv')
+  dm_coords <- as.matrix(read.csv(dm_filename, row.names=1))
+  
+  # subset Seurat object and add diffusion map reduction
+  DATAsub <- subset(DATA, cells=rownames(dm_coords))
+  dm_coords <- dm_coords[match(colnames(DATAsub), rownames(dm_coords)), ]  # ensure same cell ordering
+  DATAsub@reductions[["dm"]] <- CreateDimReducObject(embeddings=dm_coords, key="DC_", assay='RNA')
+    
+  # generate plot showing UMAP and first several diffusion components
+  umap_plot <- list(DimPlot(DATAsub, dims=1:2, reduction='umap', group.by=clustering_use, pt.size=0.1, label=T) +
+                      theme(legend.position='none'))
+  plot_nums <- as.list(seq(min(5, ncol(DATA@reductions$dm)-1)))
+  plot_list <- lapply(plot_nums, function(x) { 
+    DimPlot(DATAsub, dims=x:(x+1), reduction='dm', group.by=clustering_use, pt.size=0.1, ncol=3, label=T) +
+      ggplot2::theme(legend.position='none')})
+  p <- cowplot::plot_grid(plotlist=c(umap_plot, plot_list), ncol=3)
+  ggplot2::ggsave(p, filename='diffusion_map_components.png', path=tdir, dpi=300,
+                  units='mm', width=120*3, height=100*2, limitsize=F)
+  
+  
+  # for trajectory runs with specified start and end points
+  if (!(traj_summary_tab$Start[i] == 'auto') & !(traj_summary_tab$End[i] == 'auto')) {
+    
+    # generate UMAP with pseudotime color overlay for specified trajectory curve
+    curves <- readRDS(paste0(tdir, 'curves_object.rds'))
+    pseudo_time <- read.csv(paste0(tdir, 'trajectory_curves_pseudotime.csv'), row.names=1)
+    keep_curve <- which(lineages@slingParams$end.given)
+    
+    pal <- viridis(100, end=0.95)
+    dimred <- reducedDims(curves)
+    colors <- pal[cut(pseudo_time[,keep_curve], breaks=100)]
+    tmp <- curves; tmp@curves <- tmp@curves[keep_curve]
+    
+    png(filename=paste0(tdir, 'trajectory_curve', keep_curve, '_pseudotime.png'), res=300, units='mm', width=120, height=120)
+    par(mgp=c(0.5, 0, 0), mar=c(2,2,2,2), cex=1.3)
+    plot(dimred, xlim=range(dimred[,1]), ylim=range(dimred[,2]), col='grey85', pch=16, cex=0.3, yaxt='n', xaxt='n')
+    par(new=T)
+    plot(dimred, xlim=range(dimred[,1]), ylim=range(dimred[,2]), col=colors, pch=16, cex=0.3, main='Trajectory Pseudotime', yaxt='n', xaxt='n')
+    lines(tmp, lwd=3, col='black', type='curves')
+    box(lwd=1.5)
+    invisible(dev.off())
+  }
+}
 
-# show cells in diffusion map (DM) space
-plot_nums <- as.list(1:min(5, ncol(DATA@reductions$dm)-1))
-plot_list <- lapply(plot_nums, function(x) { 
-  DimPlot(DATA, dims=x:(x+1), reduction='dm', group.by=clustering_use, pt.size=0.1, ncol=3, label=T) +
-    ggplot2::theme(legend.position='none')})
-p <- cowplot::plot_grid(plotlist = c(umap_plot, plot_list), ncol=3)
-ggplot2::ggsave(p, filename='diffusion_map_components.png', path=tdir, dpi=300,
-                units='mm', width=120*3, height=100*2, limitsize=F)
-
-
-
-
-
-
-
+# write trajectory summary table to csv
+write.csv(traj_summary_tab, file=paste0(summary_dir, 'trajectory_runs_summary.csv'))
+          
 
 
