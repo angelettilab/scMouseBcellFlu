2816e13bbb5c6abae6a0a0cd9817f15c6483797c
Jonathan Robinson <jonrob@chalmers.se>
2020-05-22 17:34:19 +0200

    update run_trajectory.sh with changes to function and clustering

--- scripts/run_trajectory.sh
+++ scripts/run_trajectory.sh
@@ -3,8 +3,11 @@
 ########################
 ### DEFINE VARIABLES ###
 ########################
-main='/home/jonrob/projects/d_angeletti_1910'
+# main='/home/jonrob/projects/d_angeletti_1910'
+main='/cephyr/users/jonrob/Hebbe/projects/d_angeletti_1910'
 # main='/Users/jonrob/Documents/NBIS/LTS_projects/d_angeletti_1910'
+
+seurat_obj_path=$main/'analysis/06_cluster_scale/seurat_object.rds'
 
 cd $main
 # mkdir analysis
@@ -25,14 +28,16 @@ source activate trajectory-env  # linux/unix (cluster)
 ################################
 
 run_name='trajectory_01'
-Rscript $main/scripts/trajectory_inference.R \
---Seurat_object_path $main/'analysis/04_cluster/seurat_object.rds' \
---reduction_use 'dm' \
---reduction_visualize 'umap' \
---destiny_params 'k=30, n_eigs=20' \
---cluster_use 'louvain_0.95,2,4,5,7,8,9,11,13,14,15' \
---start_cluster '13' \
---end_cluster '14' \
+mkdir $main'/analysis/'$run_name
+Rscript $main/scripts/trajectory_slingshot_tradeSeq.R \
+--Seurat_object_path $seurat_obj_path \
+--pre_dim_reduct 'mnn' \
+--dim_reduct_use 'dm' \
+--dim_reduct_vis 'umap' \
+--diffusion_params 'k=30, n_eigs=20' \
+--cluster_use 'louvain_0.9,0,1,4,5,6,8,10,11,12' \
+--start_cluster '0' \
+--end_cluster '12' \
 --diff_testing 'true' \
 --assay 'RNA' \
 --output_path $main/'analysis/'$run_name \
@@ -40,14 +45,16 @@ Rscript $main/scripts/trajectory_inference.R \
 
 
 run_name='trajectory_01b'
-Rscript $main/scripts/trajectory_inference.R \
---Seurat_object_path $main/'analysis/04_cluster/seurat_object.rds' \
---reduction_use 'dm' \
---reduction_visualize 'umap' \
---destiny_params 'k=30, n_eigs=20' \
---cluster_use 'louvain_0.95,2,4,5,7,8,9,11,12,13,14,15' \
---start_cluster '13' \
---end_cluster '14' \
+mkdir $main'/analysis/'$run_name
+Rscript $main/scripts/trajectory_slingshot_tradeSeq.R \
+--Seurat_object_path $seurat_obj_path \
+--pre_dim_reduct 'mnn' \
+--dim_reduct_use 'dm' \
+--dim_reduct_vis 'umap' \
+--diffusion_params 'k=30, n_eigs=20' \
+--cluster_use 'louvain_0.9,0,1,4,5,6,8,9,10,11,12' \
+--start_cluster '0' \
+--end_cluster '12' \
 --diff_testing 'true' \
 --assay 'RNA' \
 --output_path $main/'analysis/'$run_name \
@@ -56,13 +63,15 @@ Rscript $main/scripts/trajectory_inference.R \
 
 # Note: we want to try and force this trajectory through GC somehow.
 run_name='trajectory_02'
-Rscript $main/scripts/trajectory_inference.R \
---Seurat_object_path $main/'analysis/04_cluster/seurat_object.rds' \
---reduction_use 'dm' \
---reduction_visualize 'umap' \
---destiny_params 'k=30, n_eigs=20' \
---cluster_use 'louvain_0.95,2,3,4,5,7,8,9,11,13,15' \
---start_cluster '13' \
+mkdir $main'/analysis/'$run_name
+Rscript $main/scripts/trajectory_slingshot_tradeSeq.R \
+--Seurat_object_path $seurat_obj_path \
+--pre_dim_reduct 'mnn' \
+--dim_reduct_use 'dm' \
+--dim_reduct_vis 'umap' \
+--diffusion_params 'k=30, n_eigs=20' \
+--cluster_use 'louvain_0.9,0,1,3,4,5,6,8,10,11' \
+--start_cluster '0' \
 --end_cluster '3' \
 --diff_testing 'true' \
 --assay 'RNA' \
@@ -72,13 +81,15 @@ Rscript $main/scripts/trajectory_inference.R \
 
 # Note: we want to try and force this trajectory through GC somehow.
 run_name='trajectory_02b'
-Rscript $main/scripts/trajectory_inference.R \
---Seurat_object_path $main/'analysis/04_cluster/seurat_object.rds' \
---reduction_use 'dm' \
---reduction_visualize 'umap' \
---destiny_params 'k=30, n_eigs=20' \
---cluster_use 'louvain_0.95,2,3,4,5,7,8,9,11,12,13,15' \
---start_cluster '13' \
+mkdir $main'/analysis/'$run_name
+Rscript $main/scripts/trajectory_slingshot_tradeSeq.R \
+--Seurat_object_path $seurat_obj_path \
+--pre_dim_reduct 'mnn' \
+--dim_reduct_use 'dm' \
+--dim_reduct_vis 'umap' \
+--diffusion_params 'k=30, n_eigs=20' \
+--cluster_use 'louvain_0.9,0,1,3,4,5,6,8,9,10,11' \
+--start_cluster '0' \
 --end_cluster '3' \
 --diff_testing 'true' \
 --assay 'RNA' \
@@ -87,14 +98,16 @@ Rscript $main/scripts/trajectory_inference.R \
 
 
 run_name='trajectory_03'
-Rscript $main/scripts/trajectory_inference.R \
---Seurat_object_path $main/'analysis/04_cluster/seurat_object.rds' \
---reduction_use 'dm' \
---reduction_visualize 'umap' \
---destiny_params 'k=30, n_eigs=20' \
---cluster_use 'louvain_0.95,2,4,5,7,8,9,11,12,13,15' \
---start_cluster '13' \
---end_cluster '12' \
+mkdir $main'/analysis/'$run_name
+Rscript $main/scripts/trajectory_slingshot_tradeSeq.R \
+--Seurat_object_path $seurat_obj_path \
+--pre_dim_reduct 'mnn' \
+--dim_reduct_use 'dm' \
+--dim_reduct_vis 'umap' \
+--diffusion_params 'k=30, n_eigs=20' \
+--cluster_use 'louvain_0.9,0,1,4,5,6,8,9,10,11' \
+--start_cluster '0' \
+--end_cluster '9' \
 --diff_testing 'true' \
 --assay 'RNA' \
 --output_path $main/'analysis/'$run_name \
@@ -102,14 +115,16 @@ Rscript $main/scripts/trajectory_inference.R \
 
 
 run_name='trajectory_04'
-Rscript $main/scripts/trajectory_inference.R \
---Seurat_object_path $main/'analysis/04_cluster/seurat_object.rds' \
---reduction_use 'dm' \
---reduction_visualize 'umap' \
---destiny_params 'k=30, n_eigs=20' \
---cluster_use 'louvain_0.95,2,4,5,7,8,9,11,13,15' \
---start_cluster '13' \
---end_cluster 'none' \
+mkdir $main'/analysis/'$run_name
+Rscript $main/scripts/trajectory_slingshot_tradeSeq.R \
+--Seurat_object_path $seurat_obj_path \
+--pre_dim_reduct 'mnn' \
+--dim_reduct_use 'dm' \
+--dim_reduct_vis 'umap' \
+--diffusion_params 'k=30, n_eigs=20' \
+--cluster_use 'louvain_0.9,0,1,4,5,6,8,10,11' \
+--start_cluster '0' \
+--end_cluster 'auto' \
 --diff_testing 'true' \
 --assay 'RNA' \
 --output_path $main/'analysis/'$run_name \
@@ -117,19 +132,54 @@ Rscript $main/scripts/trajectory_inference.R \
 
 
 run_name='trajectory_04b'
-Rscript $main/scripts/trajectory_inference.R \
---Seurat_object_path $main/'analysis/04_cluster/seurat_object.rds' \
---reduction_use 'dm' \
---reduction_visualize 'umap' \
---destiny_params 'k=30, n_eigs=20' \
---cluster_use 'louvain_0.95,2,4,5,7,8,9,11,12,13,15' \
---start_cluster '13' \
---end_cluster 'none' \
+mkdir $main'/analysis/'$run_name
+Rscript $main/scripts/trajectory_slingshot_tradeSeq.R \
+--Seurat_object_path $seurat_obj_path \
+--pre_dim_reduct 'mnn' \
+--dim_reduct_use 'dm' \
+--dim_reduct_vis 'umap' \
+--diffusion_params 'k=30, n_eigs=20' \
+--cluster_use 'louvain_0.9,0,1,4,5,6,8,9,10,11' \
+--start_cluster '0' \
+--end_cluster 'auto' \
 --diff_testing 'true' \
 --assay 'RNA' \
 --output_path $main/'analysis/'$run_name \
 2>&1 | tee $main/'analysis/'$run_name'/trajectory_inference_log.txt'
 
+
+run_name='trajectory_05'
+mkdir $main'/analysis/'$run_name
+Rscript $main/scripts/trajectory_slingshot_tradeSeq.R \
+--Seurat_object_path $seurat_obj_path \
+--pre_dim_reduct 'mnn' \
+--dim_reduct_use 'dm' \
+--dim_reduct_vis 'umap' \
+--diffusion_params 'k=30, n_eigs=20' \
+--cluster_use 'louvain_0.9,0,1,3,4,5,6,8,10,11,12' \
+--start_cluster '0' \
+--end_cluster '3,12' \
+--diff_testing 'true' \
+--assay 'RNA' \
+--output_path $main/'analysis/'$run_name \
+2>&1 | tee $main/'analysis/'$run_name'/trajectory_inference_log.txt'
+
+
+run_name='trajectory_05b'
+mkdir $main'/analysis/'$run_name
+Rscript $main/scripts/trajectory_slingshot_tradeSeq.R \
+--Seurat_object_path $seurat_obj_path \
+--pre_dim_reduct 'mnn' \
+--dim_reduct_use 'dm' \
+--dim_reduct_vis 'umap' \
+--diffusion_params 'k=30, n_eigs=20' \
+--cluster_use 'louvain_0.9,0,1,3,4,5,6,8,9,10,11,12' \
+--start_cluster '0' \
+--end_cluster '3,12' \
+--diff_testing 'true' \
+--assay 'RNA' \
+--output_path $main/'analysis/'$run_name \
+2>&1 | tee $main/'analysis/'$run_name'/trajectory_inference_log.txt'
 
 
 # deactivate conda environment
