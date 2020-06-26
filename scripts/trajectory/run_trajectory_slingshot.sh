b8fe531bd8e1c8d0852b040558f2a0fc8373e16b
Jonathan Robinson <jonrob@chalmers.se>
2020-06-26 16:42:22 +0200

    update run_trajectory_slingshot.sh with new results/trajectories

--- scripts/trajectory/run_trajectory_slingshot.sh
+++ scripts/trajectory/run_trajectory_slingshot.sh
@@ -3,11 +3,11 @@
 ########################
 ### DEFINE VARIABLES ###
 ########################
-# main='/home/jonrob/projects/d_angeletti_1910'
-main='/cephyr/users/jonrob/Hebbe/projects/d_angeletti_1910'
+main='/home/jonrob/projects/d_angeletti_1910'
+# main='/cephyr/users/jonrob/Hebbe/projects/d_angeletti_1910'
 # main='/Users/jonrob/Documents/NBIS/LTS_projects/d_angeletti_1910'
 
-seurat_obj_path=$main/'analysis/06_cluster_scale/seurat_object.rds'
+seurat_obj_path=$main/'analysis/06_cluster/seurat_object.rds'
 
 cd $main
 # mkdir analysis
@@ -36,16 +36,15 @@ Rscript $main/scripts/trajectory/trajectory_slingshot_tradeSeq.R \
 --dim_reduct_use 'dm' \
 --dim_reduct_vis 'umap' \
 --diffusion_params 'k=30, n_eigs=20' \
---cluster_use 'louvain_0.95,2,3,4,5,6,7,10,11,12,13' \
---start_cluster '2' \
---end_cluster '13' \
+--cluster_use 'HC_16,3,5,7,8,9,10,11,12,13,14,15,16' \
+--start_cluster '3' \
+--end_cluster '5' \
 --diff_testing 'true' \
 --assay 'RNA' \
 --output_path $main/'analysis/trajectory_slingshot/'$run_name \
 2>&1 | tee $main/'analysis/trajectory_slingshot/'$run_name'/trajectory_inference_log.txt'
 
 
-# Note: we want to try and force this trajectory through GC somehow.
 run_name='trajectory_02'
 mkdir $main'/analysis/trajectory_slingshot/'$run_name
 Rscript $main/scripts/trajectory/trajectory_slingshot_tradeSeq.R \
@@ -54,17 +53,16 @@ Rscript $main/scripts/trajectory/trajectory_slingshot_tradeSeq.R \
 --dim_reduct_use 'dm' \
 --dim_reduct_vis 'umap' \
 --diffusion_params 'k=30, n_eigs=20' \
---cluster_use 'louvain_0.95,1,2,3,4,5,6,7,10,11,12' \
---start_cluster '2' \
---end_cluster '1' \
+--cluster_use 'HC_16,3,5,8,9,10,11,12,13,14,15,16' \
+--start_cluster '3' \
+--end_cluster '5' \
 --diff_testing 'true' \
 --assay 'RNA' \
 --output_path $main/'analysis/trajectory_slingshot/'$run_name \
 2>&1 | tee $main/'analysis/trajectory_slingshot/'$run_name'/trajectory_inference_log.txt'
 
 
-# Note: we want to try and force this trajectory through GC somehow.
-run_name='trajectory_02b'
+run_name='trajectory_03'
 mkdir $main'/analysis/trajectory_slingshot/'$run_name
 Rscript $main/scripts/trajectory/trajectory_slingshot_tradeSeq.R \
 --Seurat_object_path $seurat_obj_path \
@@ -72,9 +70,9 @@ Rscript $main/scripts/trajectory/trajectory_slingshot_tradeSeq.R \
 --dim_reduct_use 'dm' \
 --dim_reduct_vis 'umap' \
 --diffusion_params 'k=30, n_eigs=20' \
---cluster_use 'louvain_0.95,1,2,3,4,5,6,7,9,10,11,12' \
---start_cluster '2' \
---end_cluster '1' \
+--cluster_use 'HC_16,3,4,5,7,8,9,10,11,12,13,14,15,16' \
+--start_cluster '4' \
+--end_cluster '5' \
 --diff_testing 'true' \
 --assay 'RNA' \
 --output_path $main/'analysis/trajectory_slingshot/'$run_name \
@@ -89,47 +87,14 @@ Rscript $main/scripts/trajectory/trajectory_slingshot_tradeSeq.R \
 --dim_reduct_use 'dm' \
 --dim_reduct_vis 'umap' \
 --diffusion_params 'k=30, n_eigs=20' \
---cluster_use 'louvain_0.95,2,3,4,5,6,7,10,11,12' \
---start_cluster '2' \
---end_cluster 'auto' \
+--cluster_use 'HC_16,3,4,5,8,9,10,11,12,13,14,15,16' \
+--start_cluster '4' \
+--end_cluster '5' \
 --diff_testing 'true' \
 --assay 'RNA' \
 --output_path $main/'analysis/trajectory_slingshot/'$run_name \
 2>&1 | tee $main/'analysis/trajectory_slingshot/'$run_name'/trajectory_inference_log.txt'
 
-
-run_name='trajectory_05'
-mkdir $main'/analysis/trajectory_slingshot/'$run_name
-Rscript $main/scripts/trajectory/trajectory_slingshot_tradeSeq.R \
---Seurat_object_path $seurat_obj_path \
---pre_dim_reduct 'mnn' \
---dim_reduct_use 'dm' \
---dim_reduct_vis 'umap' \
---diffusion_params 'k=30, n_eigs=20' \
---cluster_use 'louvain_0.95,1,2,3,4,5,6,7,10,11,12,13' \
---start_cluster '2' \
---end_cluster '1,13' \
---diff_testing 'true' \
---assay 'RNA' \
---output_path $main/'analysis/trajectory_slingshot/'$run_name \
-2>&1 | tee $main/'analysis/trajectory_slingshot/'$run_name'/trajectory_inference_log.txt'
-
-
-run_name='trajectory_05b'
-mkdir $main'/analysis/trajectory_slingshot/'$run_name
-Rscript $main/scripts/trajectory/trajectory_slingshot_tradeSeq.R \
---Seurat_object_path $seurat_obj_path \
---pre_dim_reduct 'mnn' \
---dim_reduct_use 'dm' \
---dim_reduct_vis 'umap' \
---diffusion_params 'k=30, n_eigs=20' \
---cluster_use 'louvain_0.95,1,2,3,4,5,6,7,9,10,11,12,13' \
---start_cluster '2' \
---end_cluster '1,13' \
---diff_testing 'true' \
---assay 'RNA' \
---output_path $main/'analysis/trajectory_slingshot/'$run_name \
-2>&1 | tee $main/'analysis/trajectory_slingshot/'$run_name'/trajectory_inference_log.txt'
 
 
 # deactivate conda environment
