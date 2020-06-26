84702af55fda9b38523b9cd28b542f4b4f8bfa01
Jonathan Robinson <jonrob@chalmers.se>
2020-06-26 15:00:21 +0200

    feat: improve coloring and flexibility of 3d UMAP plotting function

--- scripts/plotting/umap3d.R
+++ scripts/plotting/umap3d.R
@@ -2,21 +2,34 @@
 # 3D UMAP
 library(Seurat)
 library(plotly)
+library(htmlwidgets)
 
+# function for generating color hues (mimics default ggplot palette)
+gg_color_hue <- function(n) {
+  hues = seq(15, 375, length = n + 1)
+  hcl(h = hues, l = 65, c = 100)[1:n]
+}
+
+# load Seurat object
+DATA <- readRDS('/Users/jonrob/Documents/NBIS/LTS_projects/d_angeletti_1910/analysis/06_cluster_scale/seurat_object.rds')
+
+# run UMAP
 DATA <- RunUMAP(object = DATA, assay="RNA", reduction="mnn", dims=1:50, n.components=3, n.neighbors=50, spread=5,
-                repulsion.strength=1, min.dist=0.01, verbose=F, num_threads=0, n.epochs=500, metric = "euclidean",
-                seed.use=42, learning.rate=0.5, negative.sample.rate=5, reduction.name="umap_3d", reduction.key="umap_")
+                repulsion.strength=0.5, min.dist=0.1, verbose=F, num_threads=0, n.epochs=500, metric = "euclidean",
+                seed.use=42, learning.rate=0.5, negative.sample.rate=7, reduction.name="umap3", reduction.key="umap3_")
 
-df <- data.frame(DATA@reductions$umap_3d@cell.embeddings)
-add_data <- DATA$PlasmaCells
-add_data[add_data < 0] <- 0
-df <- data.frame(df, cluster_use=add_data)
+# extract 3D UMAP embedding into dataframe
+df <- data.frame(DATA@reductions$umap3@cell.embeddings)
+# add_data <- DATA$PlasmaCells
+# add_data[add_data < 0] <- 0
+df <- data.frame(df, cluster_use=as.factor(DATA@meta.data$louvain_0.95))
 
-pal <- c(scales::hue_pal()(8), RColorBrewer::brewer.pal(9,"Set1"), RColorBrewer::brewer.pal(8,"Set2") )
-pal <- viridisLite::magma(100)
-p_State <- plot_ly(df, x=~umap_1, y=~umap_2, z=~umap_3,color=~cluster_use,
+pal <- gg_color_hue(length(unique(df$cluster_use)))
+# pal <- c(scales::hue_pal()(8), RColorBrewer::brewer.pal(9,"Set1"), RColorBrewer::brewer.pal(8,"Set2") )
+# pal <- viridisLite::magma(100)
+p_State <- plot_ly(df, x=~umap3_1, y=~umap3_2, z=~umap3_3,color=~cluster_use,
                    colors=pal, alpha=1, alpha_stroke=0, size=0.1) %>%  add_markers() %>%
   layout(scene = list(xaxis = list(title = 'UMAP_1'), yaxis = list(title = 'UMAP_2'),zaxis = list(title = 'UMAP_3')))
 ggplotly(p_State, height=800, width=800)
-htmlwidgets::saveWidget(p_State, "/Users/jonrob/Documents/NBIS/LTS_projects/d_angeletti_1910/analysis/05_cluster_scale/umap_plots/3d_umap.html")
+saveWidget(p_State, "/Users/jonrob/Documents/NBIS/LTS_projects/d_angeletti_1910/analysis/06_cluster_scale/umap_plots/3d_umap.html")
 
