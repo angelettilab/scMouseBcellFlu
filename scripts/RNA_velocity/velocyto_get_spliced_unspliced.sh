2f7b0d2026159f4d4134eda27e5842b42fae79f8
Jonathan Robinson <jonrob@chalmers.se>
2020-08-05 15:54:46 +0200

    rename velocyto script

--- scripts/RNA_velocity/velocyto_get_spliced_unspliced.sh
+++ scripts/RNA_velocity/velocyto_get_spliced_unspliced.sh
@@ -5,7 +5,6 @@
 ########################
 main='/home/jonrob/projects/d_angeletti_1910'
 data_path='/crex/proj/snic2019-8-255/private/jay/filezila/2019/10x_cellranger_26_apr'
-# main='/Users/jonrob/Documents/NBIS/LTS_projects/d_angeletti_1910'
 annot_path=$main/'data/rna_velocity/genome_annotation_files'
 
 
@@ -14,16 +13,15 @@ cd $main
 # activate conda environment
 source activate velocyto-env
 
-# get list of sample folders in data_path
-sample_list=(`ls -d $data_path/*_SampleID_*/ | xargs -n 1 basename`)
-
 
 ####################
 ### RUN VELOCYTO ###
 ####################
 
-#velocyto run10x -m $annot_path/mm10_rmsk.gtf $main/data/rna_velocity/G19-013_SampleID_1_14mar19 $annot_path/refdata-cellranger-mm10-3.0.0/genes/genes.gtf
+# get list of sample folders in data_path
+sample_list=(`ls -d $data_path/*_SampleID_*/ | xargs -n 1 basename`)
 
+# process each sample individually
 for sample in ${sample_list[@]}
 do
     velocyto run10x -m $annot_path/mm10_rmsk.gtf $data_path/$sample $annot_path/refdata-cellranger-mm10-3.0.0/genes/genes.gtf
