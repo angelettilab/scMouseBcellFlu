24ec248857fe776ffec8123b7f2ea391719449d8
Jonathan Robinson <jonrob@chalmers.se>
2020-08-05 15:53:43 +0200

    feat: run_trajectory_slingshot.sh add two new trajectory runs

--- scripts/trajectory/run_trajectory_slingshot.sh
+++ scripts/trajectory/run_trajectory_slingshot.sh
@@ -130,6 +130,39 @@ Rscript $main/scripts/trajectory/trajectory_slingshot_tradeSeq.R \
 2>&1 | tee $main/'analysis/trajectory_slingshot/'$run_name'/trajectory_inference_log.txt'
 
 
+run_name='trajectory_07'
+mkdir $main'/analysis/trajectory_slingshot/'$run_name
+Rscript $main/scripts/trajectory/trajectory_slingshot_tradeSeq.R \
+--Seurat_object_path $seurat_obj_path \
+--pre_dim_reduct 'mnn' \
+--dim_reduct_use 'dm' \
+--dim_reduct_vis 'umap' \
+--diffusion_params 'k=30, n_eigs=20' \
+--cluster_use 'HC_16,1,2,3,4,5,6,8,9,10,11,12,13,14,15,16' \
+--start_cluster '3' \
+--end_cluster '5' \
+--diff_testing 'true' \
+--assay 'RNA' \
+--output_path $main/'analysis/trajectory_slingshot/'$run_name \
+2>&1 | tee $main/'analysis/trajectory_slingshot/'$run_name'/trajectory_inference_log.txt'
+
+
+run_name='trajectory_08'
+mkdir $main'/analysis/trajectory_slingshot/'$run_name
+Rscript $main/scripts/trajectory/trajectory_slingshot_tradeSeq.R \
+--Seurat_object_path $seurat_obj_path \
+--pre_dim_reduct 'mnn' \
+--dim_reduct_use 'dm' \
+--dim_reduct_vis 'umap' \
+--diffusion_params 'k=30, n_eigs=20' \
+--cluster_use 'HC_16' \
+--start_cluster '3' \
+--end_cluster '5' \
+--diff_testing 'true' \
+--assay 'RNA' \
+--output_path $main/'analysis/trajectory_slingshot/'$run_name \
+2>&1 | tee $main/'analysis/trajectory_slingshot/'$run_name'/trajectory_inference_log.txt'
+
 
 # deactivate conda environment
 conda deactivate
