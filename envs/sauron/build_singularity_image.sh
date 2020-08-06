0aa1536def7088fd38a223ad9785b4ebdcc1b658
Jonathan Robinson <jonrob@chalmers.se>
2020-08-06 11:39:07 +0200

    doc: fix comments specifying sylabs login

--- envs/sauron/build_singularity_image.sh
+++ envs/sauron/build_singularity_image.sh
@@ -1,12 +1,9 @@
 #! /bin/bash
 
-# To build remotely, you need to first connect to a sylabs remote server using a token.
+# To build remotely, you need to first connect to a sylabs remote server.
 # Run the following command:
-#
 #singularity remote login
-#
-# Then paste the API key (in the "sylabs-token" file) into the terminal when prompted.
-# You should see the message "API Key Verified!".
+
 
 # build image
 singularity build --remote sauron.sif Singularity_remote.def
