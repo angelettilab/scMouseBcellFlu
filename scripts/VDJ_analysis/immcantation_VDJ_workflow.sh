1598eee808442c3b43216533017969252efab525
Jonathan Robinson <jonrob@chalmers.se>
2020-09-07 17:07:06 +0200

    feat: immcantation_VDJ_workflow.sh update to incorporate light chains

--- scripts/VDJ_analysis/immcantation_VDJ_workflow.sh
+++ scripts/VDJ_analysis/immcantation_VDJ_workflow.sh
@@ -91,10 +91,10 @@ done
 #########################################################
 ### INFER IG GENOTYPE AND ESTIMATE SEQ DIST THRESHOLD ###
 #########################################################
-# Run R-script to infer sequence genotype and estimate VDJ seq hamming distance threshold
-# Results are exported into sample subfolders as "IGHV-genotyped.tab",
-# and a fasta file of the V-segment germline sequences: "IGHV_genotype.fasta".
-# A .csv file of estimated threshold values will also be written to the output directory.
+# Run R-script to infer sequence genotype and estimate heavy chain hamming distance threshold.
+# Results are exported into sample subfolders as "IGH_genotyped.tab" and "IGKL_genotyped.tab",
+# and fasta files of the V-segment germline sequences: "IGHV_genotype.fasta" and "IGKLV_genotype.fasta".
+# A .csv file of estimated threshold values is also written to the output directory.
 Rscript $main/scripts/VDJ_analysis/01_VDJ_genotype_and_threshold.R \
 --VDJ_data_path $main'/data/VDJ_OTUs' \
 --germline_path $main'/data/immcantation/germlines' \
@@ -106,6 +106,9 @@ Rscript $main/scripts/VDJ_analysis/01_VDJ_genotype_and_threshold.R \
 ############################################
 ### DEFINE CLONES AND GERMLINE SEQUENCES ###
 ############################################
+# If True, use V_CALL_GENOTYPED column generated from TIgGER. If False, the default V_CALL column is used.
+use_genotyped=False
+
 # extract sample names and threshold values (columns 1 and 2, respectively) from predicted_thresholds.csv
 samples=(`awk -F "\"*,\"*" 'FNR > 1 {print $1}' $main/'analysis/immcantation/clone_assignment/predicted_thresholds.csv'`)
 thresholds=(`awk -F "\"*,\"*" 'FNR > 1 {print $2}' $main/'analysis/immcantation/clone_assignment/predicted_thresholds.csv'`)
@@ -116,44 +119,91 @@ indx=($(seq 0 $(( ${#samples[@]} - 1 )) ))
 for i in ${indx[@]}
 do
     # Define clones based on heavy chain (dist = distance threshold)
-    # Output file is named "IGHV-genotyped_clone-pass.tab"
-    DefineClones.py -d $main'/analysis/immcantation/clone_assignment/'${samples[$i]}'/IGHV-genotyped.tab' \
+    # Output file is named "IGH_genotyped_clone-pass.tab"
+    DefineClones.py -d $main'/analysis/immcantation/clone_assignment/'${samples[$i]}'/IGH_genotyped.tab' \
     --act set \
     --model ham \
     --norm len \
     --dist ${thresholds[$i]} \
     --format changeo \
-    --outname 'IGHV-genotyped' \
-    --log $main'/analysis/immcantation/clone_assignment/'${samples[$i]}'/IGHV-genotyped_DefineClones.log'
+    --outname 'IGH_genotyped' \
+    --log $main'/analysis/immcantation/clone_assignment/'${samples[$i]}'/IGH_genotyped_DefineClones.log'
 
     # Incorporate light chain information to update the clone assignments
-    light_cluster.py -d $main'/analysis/immcantation/clone_assignment/'${samples[$i]}'/IGHV-genotyped_clone-pass.tab' \
+    # and remove cells assigned to more than one heavy chain.
+    # Note that using the "light_parse-select.tab" or "IGKL_genotyped.tab" file yields identical results.
+    light_cluster.py -d $main'/analysis/immcantation/clone_assignment/'${samples[$i]}'/IGH_genotyped_clone-pass.tab' \
     -e $main'/data/VDJ_OTUs/'${samples[$i]}'/'${samples[$i]}'_light_parse-select.tab' \
     --format changeo \
-    -o $main'/analysis/immcantation/clone_assignment/'${samples[$i]}'/IGHLK-genotyped_clone-pass.tab'
+    -o $main'/analysis/immcantation/clone_assignment/'${samples[$i]}'/IGH_genotyped_clone_corrected-pass.tab'
 
-    # Create germline sequences using genotyped sequences from TIgGER
-    # Output file is named "IGHV-genotyped_germ-pass.tab"
-    CreateGermlines.py -d $main'/analysis/immcantation/clone_assignment/'${samples[$i]}'/IGHLK-genotyped_clone-pass.tab' \
-    -g dmask \
-    --cloned \
-    -r $main'/analysis/immcantation/clone_assignment/'${samples[$i]}'/IGHV_genotype.fasta' \
-    $main/data/immcantation/germlines/imgt/mouse/vdj/imgt_mouse_IGHD.fasta \
-    $main/data/immcantation/germlines/imgt/mouse/vdj/imgt_mouse_IGHJ.fasta \
-    --vf V_CALL_GENOTYPED \
-    --format changeo \
-    --outname 'IGHLK-genotyped' \
-    --log $main'/analysis/immcantation/clone_assignment/'${samples[$i]}'/IGHV-genotyped_CreateGermlines.log'
+    if $use_genotyped
+    then
+        # Create germline sequences for heavy chains using genotyped sequences (V_CALL_GENOTYPED) from TIgGER
+        # Output file is named "IGH_genotyped_germ-pass.tab"
+        CreateGermlines.py -d $main'/analysis/immcantation/clone_assignment/'${samples[$i]}'/IGH_genotyped_clone_corrected-pass.tab' \
+        -g dmask \
+        --cloned \
+        -r $main'/analysis/immcantation/clone_assignment/'${samples[$i]}'/IGHV_genotype.fasta' \
+        $main/data/immcantation/germlines/imgt/mouse/vdj/imgt_mouse_IGHD.fasta \
+        $main/data/immcantation/germlines/imgt/mouse/vdj/imgt_mouse_IGHJ.fasta \
+        --vf V_CALL_GENOTYPED \
+        --format changeo \
+        --outname 'IGH_genotyped' \
+        --log $main'/analysis/immcantation/clone_assignment/'${samples[$i]}'/IGH_genotyped_CreateGermlines.log'
+
+        # Create germline sequences for light chains using genotyped sequences (V_CALL_GENOTYPED) from TIgGER
+        # Output file is named "IGKL_genotyped_germ-pass.tab"
+        CreateGermlines.py -d $main'/analysis/immcantation/clone_assignment/'${samples[$i]}'/IGKL_genotyped.tab' \
+        -g dmask \
+        -r $main'/analysis/immcantation/clone_assignment/'${samples[$i]}'/IGKLV_genotype.fasta' \
+        $main/data/immcantation/germlines/imgt/mouse/vdj/imgt_mouse_IGKJ.fasta \
+        $main/data/immcantation/germlines/imgt/mouse/vdj/imgt_mouse_IGLJ.fasta \
+        --vf V_CALL_GENOTYPED \
+        --format changeo \
+        --outname 'IGKL_genotyped' \
+        --log $main'/analysis/immcantation/clone_assignment/'${samples[$i]}'/IGKL_genotyped_CreateGermlines.log'
+    else
+        # Create germline sequences for heavy chains using original V_CALL columns
+        # Output file is named "IGH_genotyped_germ-pass.tab"
+        CreateGermlines.py -d $main'/analysis/immcantation/clone_assignment/'${samples[$i]}'/IGH_genotyped_clone_corrected-pass.tab' \
+        -g dmask \
+        --cloned \
+        -r $main/data/immcantation/germlines/imgt/mouse/vdj/imgt_mouse_IGHV.fasta \
+        $main/data/immcantation/germlines/imgt/mouse/vdj/imgt_mouse_IGHD.fasta \
+        $main/data/immcantation/germlines/imgt/mouse/vdj/imgt_mouse_IGHJ.fasta \
+        --format changeo \
+        --outname 'IGH_genotyped' \
+        --log $main'/analysis/immcantation/clone_assignment/'${samples[$i]}'/IGH_genotyped_CreateGermlines.log'
+
+        # Create germline sequences for light chains using original V_CALL columns
+        # Output file is named "IGKL_genotyped_germ-pass.tab"
+        CreateGermlines.py -d $main'/analysis/immcantation/clone_assignment/'${samples[$i]}'/IGKL_genotyped.tab' \
+        -g dmask \
+        -r $main/data/immcantation/germlines/imgt/mouse/vdj/imgt_mouse_IGKV.fasta \
+        $main/data/immcantation/germlines/imgt/mouse/vdj/imgt_mouse_IGLV.fasta \
+        $main/data/immcantation/germlines/imgt/mouse/vdj/imgt_mouse_IGKJ.fasta \
+        $main/data/immcantation/germlines/imgt/mouse/vdj/imgt_mouse_IGLJ.fasta \
+        --format changeo \
+        --outname 'IGKL_genotyped' \
+        --log $main'/analysis/immcantation/clone_assignment/'${samples[$i]}'/IGKL_genotyped_CreateGermlines.log'
+    fi
 done
-
 
 
 ####################################
 ### QUANTIFY VDJ MUTATION BURDEN ###
 ####################################
-# exports results as ChangeO database file "VDJseq_mutation_quant.tab"
+# heavy chain: exports results as ChangeO database file "IGH_mutation_quant.tab"
 Rscript $main/scripts/VDJ_analysis/02_VDJ_mutation_quant.R \
 --genotyped_path $main'/analysis/immcantation/clone_assignment' \
+--chain 'heavy' \
+--output_path $main'/analysis/immcantation/mutation'
+
+# light chain: exports results as ChangeO database file "IGKL_mutation_quant.tab"
+Rscript $main/scripts/VDJ_analysis/02_VDJ_mutation_quant.R \
+--genotyped_path $main'/analysis/immcantation/clone_assignment' \
+--chain 'light' \
 --output_path $main'/analysis/immcantation/mutation'
 
 
@@ -162,12 +212,12 @@ conda deactivate
 source activate Sauron.v1
 
 
-#####################################
-### ADD VDJ DATA TO SEURAT OBJECT ###
-#####################################
+#################################################################
+### MERGE HEAVY AND LIGHT CHAIN DATA AND ADD TO SEURAT OBJECT ###
+#################################################################
 Rscript $main/scripts/VDJ_analysis/03_VDJ_RNAseq_integration.R \
 --Seurat_object_path $main'/analysis/06_cluster/seurat_object.rds' \
---changeo_db_path $main'/analysis/immcantation/mutation/VDJseq_mutation_quant.tab' \
+--changeo_db_path $main'/analysis/immcantation/mutation' \
 --output_path $main'/analysis/immcantation/seurat_object_VDJannot.rds'
 
 
